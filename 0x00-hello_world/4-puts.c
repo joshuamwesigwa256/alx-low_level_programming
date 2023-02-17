a5378f0064b54934456eab8a764b5275eff7088e
Joshua Mwesigwa <joshuamwesigwa256@gmail.com>
2023-02-16 18:33:59 -0800

    100-intel second-line

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,14 @@
 
 /**
  * main - this is the entry for the programme
+ *
+ * Description: This is supposed to return 0 which is literally nothing.
+ *
  * return: 0
  */
+
 int main(void)
 {
-			
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,13 @@
-#!/bin/bash
+#include<stdio.h>
 
+/**
+ * main - The entry point
+ *
+ * Description: The programme prints a string
+ * return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
