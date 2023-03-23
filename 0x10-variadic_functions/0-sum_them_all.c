a0ce7a287ff6607eb30202f83e301959c4cd82c3
Joshua Mwesigwa <joshuamwesigwa256@gmail.com>
2023-03-23 23:24:51 +0300

    0-sum_them_all.c

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,23 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+int sum_them_all(const unsigned int n, ...)
+{
+
+	va_list list;
+	
+	unsigned int i = 0;
+	int sum = 0;
+
+	if (n == 0)
+		return 0;
+
+	va_start(list, n);
+
+	for (i = 0; i < n; i++)	
+	sum += va_arg(list, int);
+
+	va_end(list);
+
+	return sum;
+}
